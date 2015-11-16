d290d46c2ff622817748fa2b9ec5c9575ca4257a
semi-automatique <semi-automatique@users.noreply.github.com>
2015-11-15 19:24:00 -0500

    Feeplane test map for automated tests to test mm2yaml.

---
+++ tests/Testmap-for-automated-Tests.mm
@@ -0,0 +1,138 @@
+<map version="freeplane 1.3.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Testmap for automated Tests" ID="ID_152445427" CREATED="1447632712709" MODIFIED="1447632833478"><hook NAME="MapStyle">
+    <properties show_note_icons="true" show_icon_for_attributes="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="Notetest" POSITION="right" ID="_" CREATED="1160462669842" MODIFIED="1161587103055"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      This linebreak:<br />is a bold: <b>note</b>. italic <i>part </i>underlined <u>part</u> and bold italic: <i><b>bolditalic</b></i>
+    </p>
+    <ul>
+      <li>
+        a
+      </li>
+      <li>
+        b
+      </li>
+      <li>
+        c
+      </li>
+    </ul>
+    <ol>
+      <li>
+        1
+      </li>
+      <li>
+        2
+      </li>
+      <li>
+        3
+      </li>
+    </ol>
+<!--    <table style="border-left-width: 0; border-right-width: 0; border-style: solid; border-top-width: 0; width: 80%; border-bottom-width: 0" border="0">
+      <tr>
+        <td valign="top" style="border-left-width: 1; border-right-width: 1; border-style: solid; border-top-width: 1; width: 33%; border-bottom-width: 1">
+          <p style="margin-bottom: 1; margin-left: 1; margin-top: 1; margin-right: 1">
+            t11
+          </p>
+        </td>
+        <td valign="top" style="border-left-width: 1; border-right-width: 1; border-style: solid; border-top-width: 1; width: 33%; border-bottom-width: 1">
+          <p style="margin-bottom: 1; margin-left: 1; margin-top: 1; margin-right: 1">
+            t21
+          </p>
+        </td>
+        <td valign="top" style="border-left-width: 1; border-right-width: 1; border-style: solid; border-top-width: 1; width: 33%; border-bottom-width: 1">
+          <p style="margin-bottom: 1; margin-left: 1; margin-top: 1; margin-right: 1">
+            t31
+          </p>
+        </td>
+      </tr>
+      <tr>
+        <td valign="top" style="border-left-width: 1; border-right-width: 1; border-style: solid; border-top-width: 1; width: 33%; border-bottom-width: 1">
+          <p style="margin-bottom: 1; margin-left: 1; margin-top: 1; margin-right: 1">
+            t12
+          </p>
+        </td>
+        <td valign="top" style="border-left-width: 1; border-right-width: 1; border-style: solid; border-top-width: 1; width: 33%; border-bottom-width: 1">
+          <p style="margin-bottom: 1; margin-left: 1; margin-top: 1; margin-right: 1">
+            t22
+          </p>
+        </td>
+        <td valign="top" style="border-left-width: 1; border-right-width: 1; border-style: solid; border-top-width: 1; width: 33%; border-bottom-width: 1">
+          <p style="margin-bottom: 1; margin-left: 1; margin-top: 1; margin-right: 1">
+            t32
+          </p>
+        </td>
+      </tr>
+    </table> -->
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="This is a node" POSITION="right" ID="Freemind_Link_1057151065" CREATED="1160462699260" MODIFIED="1160462702810">
+<node TEXT="with a linbreak &#xa; subnode" ID="Freemind_Link_1312609197" CREATED="1160462703227" MODIFIED="1160462706665"/>
+<node TEXT="and another subnode" ID="Freemind_Link_123530478" CREATED="1160462707747" MODIFIED="1160462712132"/>
+<node TEXT="and some folded subnodes" ID="Freemind_Link_1168263765" CREATED="1160462712621" MODIFIED="1160462717788">
+<node TEXT="fold1" ID="Freemind_Link_1441123033" CREATED="1160462718297" MODIFIED="1160462720203"/>
+<node TEXT="fold2" ID="Freemind_Link_1601858134" CREATED="1160462720606" MODIFIED="1160462721786"/>
+<node TEXT="fold3" ID="Freemind_Link_1186464171" CREATED="1160462722166" MODIFIED="1160462723841"/>
+</node>
+</node>
+<node TEXT="Attributes" POSITION="right" ID="Freemind_Link_1369254551" CREATED="1160462730498" MODIFIED="1160462754008">
+<attribute NAME="attributeName" VALUE="attributeValue"/>
+</node>
+</node>
+</map>
